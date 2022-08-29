675b69a88bad71f691881d65db2bb0060e45896f
zilani131 <zilani.cuet@gmail.com>
2022-08-29 21:48:07 +0600

    sum of positive number

---
+++ module20_practice/5.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sum(int a[],int s,int t){
+ int sum=0;
+ for(int i=s-1;i<t;i++){
+        if(a[i]>0){
+            sum=sum+a[i];
+        }
+
+ }
+ cout<<sum<<" ";
+
+}
+int main(){
+
+int n;
+cin>>n;
+int a[n];
+for (int i=0;i<n;i++){
+    cin>>a[i];
+}
+int t;
+cin>>t;
+while(t--){
+    int s,x;
+    cin>>s>>x;
+
+    sum(a,s,x);
+    cout<<endl;
+
+}
+}
+
